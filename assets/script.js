19b182d7d11caa26a6dbb9d5d3ebd6a896404a52
Alexb <acbird.ab@gmail.com>
2022-05-11 20:47:21 -0600

    first commit, layout and questions started

---
+++ assets/script.js
@@ -0,0 +1,50 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+_______ is the process of finding errors and fixing them within a program.
+A. Compiling
+B. Executing
+C. Debugging
+D. Scanning
+
+What is a JavaScript element that represents True or False Values ?
+    A.Boolean
+B.String
+C.Array
+D. Number
