5e8e2efa15b2637b171ec54467d0fe41f1e9a994
Alexb <acbird.ab@gmail.com>
2022-05-12 22:10:24 -0600

    meh

--- assets/script.js
+++ assets/script.js
@@ -1,50 +1,90 @@
+var questionNumber = 0;
+var currentQ;
+var score = { total: 0, time: 30, questionsAnswered: 0 };
+var questions = [
+  {
+    question:
+      "_______ is the process of finding errors and fixing them within a program.",
+    options: ["Compiling", "Executing", "Debugging", "Scanning"],
+    answer: "Debugging",
+  },
+  {
+    question:
+      "What is a JavaScript element that represents True or False Values?",
+    options: ["Boolean", "String", "Array", "Number"],
+    answer: "Boolean",
+  },
+  {
+    question: "Question 3?",
+    options: ["Boolean", "String", "Array", "Number"],
+    answer: "Boolean",
+  },
+  {
+    question: "Question 4?",
+    options: ["Boolean", "String", "Array", "Number"],
+    answer: "Boolean",
+  },
+  {
+    question: "Question 5?",
+    options: ["Boolean", "String", "Array", "Number"],
+    answer: "Boolean",
+  },
+];
 
+$(function () {
+  score.total = questions.length - 1;
+  $("#startBtn").click(function () {
+    countdown();
+    $("#quizBody").show();
+    $("#countdown").show();
+    $("#title").hide();
+    loadQuestion();
+  });
+  $("body").on("click", ".answerBtn", function () {
+    var a = $(this).text();
+    if (a === currentQ.answer) {
+      if (score.total === score.questionsAnswered) {
+        endGame();
+      }
+      loadQuestion();
+      score.questionsAnswered++;
+    } else {
+      $("#wrong").show();
+    }
+  });
+});
 
+function endGame() {
+  $("#scorePage").show();
+  $("#quizBody").hide();
+  $("#countdown").hide();
+  $("#finalScore").text(score.time);
+}
 
+function countdown() {
+  var timerEl = $("#countdown");
+  var timeInterval = setInterval(function () {
+    if (score.time > 0) {
+      timerEl.text("Time Left: " + score.time);
+      score.time--;
+    } else {
+      endGame();
+    }
+  }, 1000);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-_______ is the process of finding errors and fixing them within a program.
-A. Compiling
-B. Executing
-C. Debugging
-D. Scanning
-
-What is a JavaScript element that represents True or False Values ?
-    A.Boolean
-B.String
-C.Array
-D. Number
+function loadQuestion() {
+  $("#wrong").hide();
+  var randomQuestion = questionNumber; // Math.floor(Math.random() * questions.length);
+  currentQ = questions[randomQuestion];
+  $("#questionEl").text(currentQ.question);
+  $("#answersEl").empty();
+  $.each(currentQ.options, function (index, value) {
+    var li =
+      "<li><button class='btn btn-primary btn-large my-2 answerBtn'>" +
+      value +
+      "</button></li>";
+    $("#answersEl").append(li);
+  });
+  questionNumber++;
+}
